8ea8e3e05f7e66abff7953e890aaae1b22b1afdf
Calvin Ronksley <45170098+blackleakz@users.noreply.github.com>
2023-08-10 23:12:13 +0000

    Adding Setup-files

--- setup_env/linux-setup.sh
+++ setup_env/linux-setup.sh
@@ -25,6 +25,7 @@ done
 
 auto() {
 echo -n "Console > Auto-installing now..."
+echo "Skipping system-check, auto-install will try all pkg-managers! "
 apt-get update;
 apt-get install -y npm;
 apt-get install -y node;
@@ -35,3 +36,34 @@ pacman -Sy npm;
 pacman -Sy node;
 pacman -Sy node.js;
 pacman -Sy nodejs;
+
+echo -n "Console > Updating npm...."
+npm install -g npm;
+
+setup_env;
+}
+
+manual() {
+
+}
+
+
+setup_env() {
+echo -n "Console > Seting up env-vars::!"
+read -p "Console > Enter path of reactjs-app::> " path
+
+cd $path
+ls -l package*
+
+echo -n "Console > Are you already in your reactjs-app-folder and you can see the package.json file right now?(Yy/Nn)"
+read -p "Enter Yy/Nn:>> " yn
+case $yn in 
+    y|Y) echo -n "Console > Continue..."; continue;;
+    n|N) echo -n "Console > You arent in your ReactJS-App folder, plz enter folder-path now!" && read -p "folder-path: " path && cd $path; continue;;
+    *) echo -n "Invalid input"; continue;;
+esac
+
+
+echo -n "Console > Installing now npm-packages!"
+npm install react-router-dom;
+npm install react-bootstrap;
